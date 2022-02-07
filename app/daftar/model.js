5be20ffab41355e0f1358967caa6cb54d2d36ed7
eco1511 <eco1511@gmail.com>
2022-02-07 11:16:32 +0700

    tambah pendaftan

--- app/daftar/model.js
+++ app/daftar/model.js
@@ -1,19 +1,27 @@
 const mongoose = require('mongoose')
 
 let daftarSchema = mongoose.Schema({
-    anamnesa: {
-        type: String,
-        require: [true, 'anamnesa harus diisi']
-    },
-    category: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Category'
+    usia: {
+        type: Number,
     },
 
-    nominals: [{
+    height: {
+        type: Number,
+    },
+
+    weight: {
+        type: Number,
+    },
+
+    pasien: [{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Nominal'
+        ref: 'Pasien'
     }],
+
+    layanan: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Layanan'
+    },
 
     user: {
         type: mongoose.Schema.Types.ObjectId,
